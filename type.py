76b46f3065ea36f64147964fff4220e2fbd9d9ca
Frieren <1692219062wang@gmail.com>
2024-03-08 01:22:12 +0800

    Operational character

---
+++ type.py
@@ -0,0 +1,22 @@
+#Operational character
+x = 10
+y = 3
+print ("x + y = ", x + y)
+print ("x - y = ", x - y)
+print ("x * y = ", x * y)
+print ("x / y = ", x / y)
+print ("x // y = ", x // y)
+print ("+x = ", +x)
+print ("-y = ", -y)
+print ("x % y = ", x % y)
+print ("x ** y = ", x ** y)
+'''
+        x += y      >>      x = x + y
+        x -= y      >>      x = x - y
+        x *= y      >>      x = x * y
+        x /= y      >>      x = x / y
+        x //= y     >>      x = x // y
+        x %= y      >>      x = x % y
+        x **= y     >>      x = x ** y
+'''
+
