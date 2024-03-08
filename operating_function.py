50ea204f76df37f55a4095728a390c26c6142a13
Frieren <1692219062wang@gmail.com>
2024-03-09 00:11:11 +0800

    operating function

---
+++ operating_function.py
@@ -0,0 +1,18 @@
+#operating function
+x = 10
+y = 3
+# x的绝对值
+print ("abs(x) = ", abs(x))
+# x除以y的商和余数
+print ("divmod(x, y) = ", divmod(x, y))
+# x的y次方取模1000
+print ("pow(x, y, 1000) = ", pow(x, y, 1000))
+# x保留d位小数
+print ("round(x, d) = ", round(x, y))
+print ("max(x, y) = ", max(x, y))
+print ("min(x, y) = ", min(x, y))
+print ("int(x) = ", int(x))
+print ("float(x) = ", float(x))
+print ("complex(x) = ", complex(x))
+
+
