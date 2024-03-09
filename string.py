f4ec2627d2a298d0407c48d8c897f37a47b3d0de
Frieren <1692219062wang@gmail.com>
2024-03-09 13:50:23 +0800

    string format

--- string.py
+++ string.py
@@ -1,28 +1,16 @@
 # coding=utf-8
-#string process method
+# string format
+
+# 槽赋值
+print("{}:计算机{}的cpu占用率为{}%".format("2018-10-10", "c", 10))
+
+# 参数序号赋值
+print("{1}:计算机{0}的cpu占用率为{2}%".format("2018-10-10", "c", 10))
 
 """
-    str.lower()
-    返回字符串的小写副本
-    str.upper()
-    返回字符串的大写副本
-
-    str.split(sep=None)
-    返回str根据sep分割的列表
-
-    str.count(sub)
-    返回字串sub在str中出现的次数
-
-    str.replace(old, new)
-    返回字符串str中所有old字串被替换为new的副本
-
-    str.center(width[,fillchar])
-    字符串str根据width居中，两边使用fillchar填充
-
-    str.strip(chars)
-    从str中去除左右侧chars中列出的字符
-
-    str.join(iter)
-    在iter变量除最后元素外每个元素最后依次增加一个str
-
+槽中的格式控制方法
+    :   <填充>  <对齐>  <宽度>  <,>  <.精度>  <类型>
+                  >                           
+                  <                           
+                  ^
 """
